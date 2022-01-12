6940fc7fc3e4b5be6c4c2c88bf47b98459e00dc6
dengxinjuan <dengxinj@gmail.com>
2022-01-11 16:28:53 -0800

    add loadmore button

--- src/SingleCategory.js
+++ src/SingleCategory.js
@@ -1,17 +1,36 @@
 import ProductCard from "./ProductCard";
+import { useState } from "react";
+
+const LIMIT = 4; // it is the everytime load 4 more
 
 const SingleCategory = ({ category, data }) => {
   const filtedData = data.filter((x) => x.product_name == category);
   //console.log(filtedData);
+  const [list, setList] = useState(filtedData.slice(0, LIMIT)); // we set the current list
+  const [index, setIndex] = useState(LIMIT);
+  const [showMore, setShowMore] = useState(true);
+  const LENGTH = Object.keys(filtedData).length;
+
+  /**load more function */
+  const loadMore = () => {
+    const newIndex = index + LIMIT;
+    const newShowMore = newIndex < LENGTH - 1;
+    const newList = filtedData.slice(index, newIndex);
+    setIndex(newIndex);
+    setList(newList);
+    setShowMore(newShowMore);
+  };
+
   return (
     <div>
       {category}
 
       <hr />
       <div className="category">
-        {filtedData.map((product) => (
+        {list.map((product) => (
           <ProductCard productData={product} />
         ))}
+        {showMore && <button onClick={loadMore}>Load More!</button>}
       </div>
     </div>
   );
